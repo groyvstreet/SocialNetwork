c0e1856ff4df32e20fd3fc266592e1eb6bdc44b8
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-08-31 21:39:05 +0300

    some updates

--- SocialNetwork/src/services/PostService/PostService.Grpc/Program.cs
+++ SocialNetwork/src/services/PostService/PostService.Grpc/Program.cs
@@ -1,17 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using PostService.Application.Interfaces;
+using PostService.Domain.Entities;
 using PostService.Grpc.Services;
+using PostService.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var assemblyName = builder.Configuration.GetSection("MigrationsAssembly").Get<string>();
+builder.Services.AddDbContext<DataContext>(
+    opt => opt.UseNpgsql(connectionString,
+                         npgsqlOpt => npgsqlOpt.MigrationsAssembly(assemblyName)));
 
-// Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+builder.Services.AddTransient<IRepository<UserProfile>, Repository<UserProfile>>();
+builder.Services.AddTransient<IRepository<Post>, Repository<Post>>();
+
+builder.Services.AddTransient<IPostService, PostService.Application.Services.PostService>();
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<GrpcPostService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context = services.GetRequiredService<DataContext>();
+
+    if (context.Database.GetPendingMigrations().Any())
+    {
+        context.Database.Migrate();
+    }
+}
 
 app.Run();
--- SocialNetwork/src/services/PostService/PostService.Grpc/Services/GrpcPostService.cs
+++ SocialNetwork/src/services/PostService/PostService.Grpc/Services/GrpcPostService.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Grpc.Core;
+using PostService.Application.DTOs.PostDTOs;
 using PostService.Application.Interfaces;
 
 namespace PostService.Grpc.Services
@@ -7,12 +8,15 @@ namespace PostService.Grpc.Services
     public class GrpcPostService : GrpcPost.GrpcPostBase
     {
         private readonly ILogger<GrpcPostService> logger;
+        private readonly IMapper mapper;
         private readonly IPostService postService;
 
         public GrpcPostService(ILogger<GrpcPostService> logger,
+                               IMapper mapper,
                                IPostService postService)
         {
             this.logger = logger;
+            this.mapper = mapper;
             this.postService = postService;
         }
 
@@ -20,10 +24,27 @@ namespace PostService.Grpc.Services
         {
             var posts = await postService.GetPostsAsync();
             var postList = new PostList();
-            var mappedPosts = posts.Select().ToList();
+            var mappedPosts = posts.Select(mapper.Map<Post>).ToList();
             postList.Posts.AddRange(mappedPosts);
 
-            return Task.FromResult(posts);
+            return postList;
+        }
+
+        public override async Task<Post> Get(PostId postId, ServerCallContext context)
+        {
+            var id = Guid.Parse(postId.Id);
+            var post = await postService.GetPostByIdAsync(id);
+            var mappedPost = mapper.Map<Post>(post);
+
+            return mappedPost;
+        }
+
+        public override async Task<Empty> Add(Post post, ServerCallContext context)
+        {
+            var mappedPost = mapper.Map<AddPostDTO>(post);
+            await postService.AddPostAsync(mappedPost);
+
+            return new Empty();
         }
     }
 }
