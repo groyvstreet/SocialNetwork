2f9b809557edb97d9c91869d714359e106e1f4e0
groyvstreet <kirya.polyakov.2003@mail.ru>
2023-09-10 14:38:31 +0300

    exception handler middleware updated

--- SocialNetwork/src/services/PostService/PostService.API/Middlewares/ExceptionHandlerMiddleware.cs
+++ SocialNetwork/src/services/PostService/PostService.API/Middlewares/ExceptionHandlerMiddleware.cs
@@ -17,17 +17,22 @@ namespace PostService.API.Middlewares
             {
                 await next(context);
             }
-            catch (NotFoundException ex)
+            catch (Exception ex)
             {
+                switch (ex)
+                {
+                    case NotFoundException:
+                        context.Response.StatusCode = 404;
+                        break;
+                    case AlreadyExistsException:
+                        context.Response.StatusCode = 409;
+                        break;
+                    default:
+                        context.Response.StatusCode = 500;
+                        break;
+                }
+
                 context.Response.Headers.ContentType = "text/json; charset=utf-8";
-                context.Response.StatusCode = 404;
-                var response = new { ex.Message };
-                await context.Response.WriteAsJsonAsync(response);
-            }
-            catch (AlreadyExistsException ex)
-            {
-                context.Response.Headers.ContentType = "text/json; charset=utf-8";
-                context.Response.StatusCode = 409;
                 var response = new { ex.Message };
                 await context.Response.WriteAsJsonAsync(response);
             }
